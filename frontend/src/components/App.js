5d9d5bef1f5b6bd7ad63be4f68156e04fe1f9ffa
Aline Alencar <aline.alencar@dataprev.gov.br>
2018-03-13 06:50:30 -0300

    Fix: adjust category param. Add PostDetail import in App.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,7 +5,7 @@ import Menu from './Menu';
 import NewPost from './NewPost';
 import Category from './Category';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import PostDetail from './PostDetail';
+import PostDetail from './PostDetail';
 
 class App extends Component {
   render() {
@@ -16,12 +16,6 @@ class App extends Component {
             <Menu />
             <Route exact path="/" component={Home} />
             <Route path="/addPost" component={NewPost} />
-            {/* <Route path="/react" render={() => <Category category="react" />} />
-          <Route path="/redux" render={() => <Category category="redux" />} />
-          <Route
-          path="/udacity"
-          render={() => <Category category="udacity" />}
-        /> */}
             <Route path="/:category" component={Category} />
             <Route path="/postDetail/:postId" component={PostDetail} />
           </div>
