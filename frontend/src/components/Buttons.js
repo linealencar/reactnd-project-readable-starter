b91c02b2e97b6cae68b61499d94227d6f4f8866d
linealencar <linealencar@gmail.com>
2018-04-25 12:26:34 +0000

    feat: include FlatButton in Buttons.js

--- frontend/src/components/Buttons.js
+++ frontend/src/components/Buttons.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { orderPosts } from '../actions';
 import { connect } from 'react-redux';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
 
 class Buttons extends Component {
   orderPosts = sortingType => {
@@ -9,6 +11,12 @@ class Buttons extends Component {
   render() {
     return (
       <div className="App">
+        <FlatButton
+          href="https://github.com/callemall/material-ui"
+          target="_blank"
+          secondary={true}
+          icon={<FontIcon className="muidocs-icon-custom-github" />}
+        />
         <button
           className="icon-btn"
           onClick={() => this.orderPosts('voteScore')}
